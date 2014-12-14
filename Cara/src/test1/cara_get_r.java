7d08e7214a9ce86cd5281f96c2cf9176714dc9d2
linsfield <linsfield@163.com>
2014-12-15 01:21:48 +0800

    Update cara_get_r.java

--- Cara/src/test1/cara_get_r.java
+++ Cara/src/test1/cara_get_r.java
@@ -3,8 +3,8 @@ import java.lang.Math;
 
 public class cara_get_r {
 
-	final double MIN=0.0;
-	final double MAX=999999.0;
+	final double MIN = 0.0;
+	final double MAX = 100000000.0;
 
 	private double r;
 	private double tempr;
@@ -19,9 +19,9 @@ public class cara_get_r {
 		switch(flag) {
 		case 1:
 			/* first model here */
-			n = frequency * frequency / (81.0 * 4);
-			tempr = ( MIN + MAX ) / 2;
-			temp = 1.36 * Math.pow(tempr, (-2.1)) + 168 * Math.pow(tempr, (-6.13));
+			n = frequency * frequency / (81.0 * 4.0);
+			tempr = ( MIN + MAX ) / 2.0;
+			temp = (1.36 * Math.pow(tempr, (-2.1)) + 168.0 * Math.pow(tempr, (-6.13))) * Math.pow(10, 6);
 			while( Math.abs( n-temp ) >= 0.0001 ) {
 				if( n > temp ) {
 					max = tempr;
@@ -31,10 +31,10 @@ public class cara_get_r {
 					min = tempr;
 					tempr = ( min + max ) / 2.0;
 				}
-				temp = 1.36 * Math.pow(tempr, (-2.1)) + 168.0 * Math.pow(tempr, (-6.13));
+				temp = 1.36 * Math.pow(tempr, (-2.1)) + 168.0 * Math.pow(tempr, (-6.13)) * Math.pow(10, 6);
 			}
 
-		    r = tempr * 6.955 * Math.pow(10,8);
+		    r = tempr * 6.955 * Math.pow(10,5);
 			break;
 		case 2:
 			/* second model here */
