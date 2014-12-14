f27cc68b8d78cc57323cfa3432c08d036ab8e888
skull591 <382340878@qq.com>
2014-12-14 21:55:05 +0800

    Update cara_cal.java

--- Cara/src/test1/cara_cal.java
+++ Cara/src/test1/cara_cal.java
@@ -1,21 +1,37 @@
-package cara_calculcate;
+package com.alex.image_processing;
 
 public class cara_cal {
+	     double ans;
+		 double r1;
+		 double r2;
+		cara_get_r cr;
+		cara_get_v cv;
+		double frequency1;
+		double frequency2;
+		int timestart;
+		int timeend;
+		
+		public cara_cal(double frequency1, double frequency2, int timestart, int timeend){
+			cr = new cara_get_r();
+			cv = new cara_get_v();
+			this.frequency1=frequency1;
+			this.frequency2=frequency2;
+			this.timestart=timestart;
+			this.timeend=timeend;
+		}
+		public void change(double frequency1, double frequency2, int timestart, int timeend){
+			this.frequency1=frequency1;
+			this.frequency2=frequency2;
+			this.timestart=timestart;
+			this.timeend=timeend;
+		}
+		public double calculcate(int flag) {
+			r1 = cr.calculcateR(flag, frequency1);
+			r2 = cr.calculcateR(flag, frequency2);
+			ans = cv.calculcateV(r1, r2, timestart, timeend);
+			return ans;
+		}
+		
 
-	private static double ans;
-	static double r1;
-	static double r2;
-	
-	public static double calculcate(int flag, double frequency1, double frequency2, int timestart, int timeend) {
-		r1 = cara_get_r.calculcateR(flag, frequency1);
-		r2 = cara_get_r.calculcateR(flag, frequency2);
-		ans = cara_get_v.calculcateV(r1, r2, timestart, timeend);
-		return ans;
-	}
-	
-	/* test */
-	/*public static void main(String[] args)
-	{
-		System.out.println(calculcate(1, 1437.5, 1627.3, 1005, 1023));
-	}*/
 }
+
