c3ea8f8399a09d4bcdd5c9dade4ae2a954850520
linsfield <linsfield@163.com>
2015-01-03 07:00:46 +0800

    Update cara_get_v.java

--- Cara/src/calc/cara_get_v.java
+++ Cara/src/calc/cara_get_v.java
@@ -5,7 +5,7 @@ public class cara_get_v {
 	
 	public double calculcateV(double r1, double r2, int timestart, int timeend) {
 		v = Math.abs(r1 - r2) / Math.abs(timestart - timeend);
-		return v;
+		return 2 * v;
 	}
 
 }
